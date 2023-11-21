de3385a048fd0091c43981a1f79263bd4e4a0962
Airamek <vegkeppnemairamek@gmail.com>
2023-11-21 09:07:54 +0100

    Initial commit

---
+++ Assets/Scripts/FlyBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rigidbody2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            _rigidbody2.velocity = Vector2.up * this._velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidbody2.velocity.y * _rotationSpeed);
+    }
+}
